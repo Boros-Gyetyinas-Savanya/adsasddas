c02193bede2bab739d43ddf1f0cb74e0f9cd05dc
savger1024 <90380477+savger1024@users.noreply.github.com>
2022-10-06 20:38:01 +0200

    Kezdőlap, Formok közötti ugrálás
    Innen indul az app. Meg van oldva minden oldalnál, hogy a megfelelő gombra kattintva átugorjon a megfelelő form-ra.

---
+++ Kezdolap.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Jelmez2
+{
+    public partial class Kezdolap : Form
+    {
+        public Kezdolap()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form1 Form1 = new Form1(); Form1.Show(); this.Hide();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ujjelmez ujjelmez= new ujjelmez(); ujjelmez.Show(); this.Hide();
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Jelmezmod Jelmezmod = new Jelmezmod(); Jelmezmod.Show(); this.Hide();
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Torles Torles = new Torles(); Torles.Show(); this.Hide();
+        }
+    }
+}
