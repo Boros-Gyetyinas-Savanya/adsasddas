e6b1eb88fab92c1f93c663116296143cd8b2384c
savger1024 <90380477+savger1024@users.noreply.github.com>
2022-10-13 11:37:41 +0200

    Működik!

--- Táblázat.cs
+++ Táblázat.cs
@@ -17,41 +17,40 @@ namespace Jelmez2
         {
             InitializeComponent();
 
-            int ev = DateTime.Now.Year;
-            int honap = DateTime.Now.Month;
-            int nap= DateTime.Now.Day;
+            DateTime now = new DateTime();
 
-            int[] kezdoIdo = new int[Form1.kolcsonzesList.Count];
-            int[] mostIdo = new int[Form1.kolcsonzesList.Count]; 
-            int[] elteltIdo = new int[Form1.kolcsonzesList.Count]; 
-            int[] hatraIdo = new int[Form1.kolcsonzesList.Count];
-            int sorsz = 0;
+
+
 
             foreach (var item in Form1.kolcsonzesList)
             {
-                kezdoIdo[sorsz] = item.ev * 365 + item.honap * 30 + item.nap;
-                mostIdo[sorsz] = ev * 365 + honap * 30 + nap;
-                elteltIdo[sorsz] = mostIdo[sorsz] - kezdoIdo[sorsz];
-                hatraIdo[sorsz] = item.napokSzama - elteltIdo[sorsz];
-                if (hatraIdo[sorsz] < 0)
+                int ev = DateTime.Now.Year;
+                int honap = DateTime.Now.Month;
+                int nap = DateTime.Now.Day;
+                int elteltIdo = DateTime.Now.Year * 365 - item.ev * 365 + DateTime.Now.Month * 30 - item.honap * 30 + DateTime.Now.Day - item.nap;
+                int idoHatra = item.napokSzama - elteltIdo;
+
+                if (idoHatra < 0)
                 {
-
-
-                    this.dataGridView1.Rows.Add(item.jelmez.jelmezNev, $"{item.jelmez.minMeret}-{item.jelmez.maxMeret}", item.jelmez.napiAr, item.db, item.napokSzama * item.db * item.jelmez.napiAr, item.ugyfel.nev, item.ugyfel.cim, item.ugyfel.adoszam, item.ugyfel.email, "Késés!", hatraIdo[0] * -1);
+                    this.dataGridView1.Rows.Add(item.jelmez.jelmezNev, $"{item.jelmez.minMeret}-{item.jelmez.maxMeret} cm", item.jelmez.napiAr, item.db, item.napokSzama * item.db * item.jelmez.napiAr, item.ugyfel.nev, item.ugyfel.cim, item.ugyfel.adoszam, item.ugyfel.email, "Késés!", idoHatra);
                 }
                 else
                 {
-                    dataGridView1.Rows.Add(item.jelmez.jelmezNev, $"{item.jelmez.minMeret}-{item.jelmez.maxMeret}", item.jelmez.napiAr, item.db, item.napokSzama * item.db * item.jelmez.napiAr, item.ugyfel.nev, item.ugyfel.cim, item.ugyfel.adoszam, item.ugyfel.email, hatraIdo[0], "nincs");
+                    this.dataGridView1.Rows.Add(item.jelmez.jelmezNev, $"{item.jelmez.minMeret}-{item.jelmez.maxMeret} cm", item.jelmez.napiAr, item.db, item.napokSzama * item.db * item.jelmez.napiAr, item.ugyfel.nev, item.ugyfel.cim, item.ugyfel.adoszam, item.ugyfel.email, idoHatra, "nincs");
                 }
-                sorsz++;
 
+                
             }
-
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             Kezdolap kezdolap = new Kezdolap(); kezdolap.Show(); this.Hide();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
         }
     }
 }
